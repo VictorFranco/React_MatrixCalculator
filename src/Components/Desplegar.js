7ca42e271bb5a5964edf71bffa5e3ba24a1a7d5a
Roy Vega <royvega1970@gmail.com>
2021-06-12 15:15:06 -0500

    Limpieza de codigo

--- src/Components/Desplegar.js
+++ src/Components/Desplegar.js
@@ -1,67 +1,86 @@
 import React, {Component} from 'react';
 
-// function Matriz(size){
-//     var matriz = new Array(size);
-//         for(i = 0; i < size ; i++){
-//                matriz[i]=new Array(size);
-
-//          }
-//     return matriz;
-// }
 
 
 
-function Itemdos(props) {
+
+
+// function Itemdos(props) {
+
+//   let Des = new Desplegar()
+  
     
-    
-    return (<li><input value={props.message}/></li>);
-  }
-
-function Item(props) {
-    
-       return (props.message.map((message) => <Itemdos key={message} message={message}/>))
-  }
-
-class Desplegar extends Component{
-
-//     constructor(props)
-//   { 
-//       super (props)
-//       this.state = {tamano: 4}
-      
-
+//     return (<input type="number"  name={props.message} />);
+   
 //   }
 
+// function Item(props) {
+    
+//        return (props.message.map((message) => <Itemdos key={message} message={message}/>))
+//   }
 
+class Desplegar extends Component{
    
+  constructor(props)
+  { 
+     let arr = new Array(9);
+      super(props)
+      this.state ={arr}
+     
+  }
 
-    
-    
+ Itemdos(matriz) {
+  
+  return(<input type="number" key={matriz} value={matriz}/>)   
+    }
+  
+  Item(matriz) {
       
+    return(matriz.map(message => this.Itemdos(message)))
+         
+    }
+
+  onSubmit(e){
+    e.preventDefault()
+    const arr = new Array(3)
+    let count = 0;
+    for(let i=0; i<3 ;i++){
+      arr = new Array(3)
+      for(let j = 0; i<3; i++){
+        console.log(e.taget)
+        count += 1
+      }
+    }
     
+  }
+
+
+ 
+  
 
     render(){
        
-        let rev = this.props.tamano
-         if(rev == false || rev == 0){
+       let rev = this.props.tamano
+         if(rev){
             let size = parseInt(rev)
        
-            console.log(rev )
                                 
           
-            // let size = 3
+            let m = 0;
             var matriz = new Array(size);
             for(let i = 0; i < size ; i++){
                    matriz[i]=new Array(size);
                    for(let j = 0; j< size ; j++){
-                    matriz[i][j] = "A"+j+"B"+i;
+                    matriz[i][j] = m;
+                    m++;
                    }
-    
+                   
              }
             return (
-                <ul>
-                  {matriz.map((message) => <Item key={message} message={message} />)}
-                </ul>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                  {matriz.map(message => this.Item(message))}
+                  <button type="submit">Resolver</button>
+                </form>
               );
          } else {
              return (
